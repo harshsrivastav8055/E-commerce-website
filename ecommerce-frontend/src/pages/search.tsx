3ca901aee13293c900194382cd0664f9367aad32
harshsrivastav8055 <srivastavaharsh8055@gmail.com>
2024-05-29 01:12:03 +0530

    new changes

--- ecommerce-frontend/src/pages/search.tsx
+++ ecommerce-frontend/src/pages/search.tsx
@@ -1,7 +1,14 @@
 const Search = () => {
   return (
     <div>
-      Search
+     esse exercitationem libero ad asperiores eius pariatur, ipsum necessitatibus velit aliquam nihil voluptates doloremque numquam!as, nemo, sint assumenda aut quo voluptas nobis quae magnam? Vero quidem necessitatibus quibusdam exercitationem voluptatibus voluptatem quas et recusandae beatae rerum?
+      Magni, laudantium quia, in, facere iste at aspernatur eveniet quasi necessitatibus nesciunt assumenda sapiente incidunt. Tenetur nobitus nostrum placeat iusto magnam aliquam ipsum! Eum, voluptas.
+      Debitis temporibus necessitatibus quis minus deserunt quam, facere, ipsa, aspernatur ratione dignissimos corporis molestias dolore. Iste maiores odio eius aliquid, harum ex molestiae repellat magnam? Error nulla non aut at.
+      Eaque pariatur vero sunt dolorem in, necessitatibus laudantium aut enim. Aliquid iure, vitae vero voluptates maiores accusamus, necessitatibus nostrum facere a quasi doloribus, corrupti beatae. Voluptatum a corrupti quae consequatur.
+      Nam reprehenderit libero quas rerum a saepe facere error incidunt aperiam aspernatur, aliquid perferendis nisi iste nulla velit! Eius, repellendus explicabo. Iste cupiditate ut blanditiis vitae ipsa eveniet minus nostrum.
+      Doloribus dolore exercitationem quam suscipit placeat facere fugiat architecto. Tempore eligendi ut incidunt repudiandae culpa nobis. Non minus nisi, deleniti suscipit perspiciatis hic error placeat illum sunt soluta vitae odit.
+      Doloremque, minima aspernatur sapiente, iusto perferendis impedit dolor illum nam animi non possimus similique expedita ratione totam. Nemo consequuntur rem modi iure id ab aut eaque, officiis mollitia doloremque aperiam!
+      Odit exercitationem itaque asperiores? Et id porro mollitia accusantium eligendi minima eaque magni est adipisci, laudantium nisi officia quaerat reprehenderit, eius soluta. Vel beatae tempore soluta provident et. Tempora, dolor.
     </div>
   )
 }
